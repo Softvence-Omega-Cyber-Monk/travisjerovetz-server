7093ab6f710db2110fc2076c4ddb80d81c20bc3a
mahdijihad001 <mahdijihad001@gmail.com>
2025-12-17 14:03:58 +0600

    Update User analatycs Api

--- src/app/module/analytics/analytics.route.ts
+++ src/app/module/analytics/analytics.route.ts
@@ -8,7 +8,7 @@ const analyticsRouter = Router();
 
 
 analyticsRouter.get("/admin/analytics", checkAuths(IRole.ADMIN), analyticsController.adminAnalytics);
-analyticsRouter.get("/user/analytics/:userId", checkAuths(), analyticsController.userAnalyticsData);
+analyticsRouter.get("/user/analytics", checkAuths(), analyticsController.userAnalyticsData);
 
 
 
