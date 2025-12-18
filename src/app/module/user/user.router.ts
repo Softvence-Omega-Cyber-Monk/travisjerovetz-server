c985a79d940e0bcc550b76d01d8c57ed2c1dbc7b
mahdijihad001 <mahdijihad001@gmail.com>
2025-12-18 19:05:19 +0600

    Fix Refresh Token Problem

--- src/app/module/user/user.router.ts
+++ src/app/module/user/user.router.ts
@@ -7,7 +7,7 @@ const UserRouter = Router();
 
 UserRouter.post("/signIn", UserController.SingIn);
 UserRouter.post("/signUp", UserController.SignUp);
-UserRouter.get("/refreshToken", UserController.getAccessTokenUseRefreshToken)
+UserRouter.post("/refreshToken", UserController.getAccessTokenUseRefreshToken)
 UserRouter.get("/get/allUser", UserController.getAllUser);
 UserRouter.get("/getMe" , checkAuths() , UserController.getMe);
 UserRouter.delete("/delete/:userId" , checkAuths() , UserController.deleteUser);
